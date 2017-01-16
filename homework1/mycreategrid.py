8599ae6d2137e63b9dbe12e3d72b0d801659bd1e
bmawji3 <bmawji3@gatech.edu>
2017-01-16 15:40:55 -0500

    cleanup, need to go to TA

--- homework1/mycreategrid.py
+++ homework1/mycreategrid.py
@@ -28,80 +28,28 @@ def myCreateGrid(world, cellsize):
     grid = None
     dimensions = (0, 0)
     ### YOUR CODE GOES BELOW HERE ###
-    # print ('points', world.getPoints())
     maxX = world.dimensions[0]
     maxY = world.dimensions[1]
     dimensions = (maxX, maxY)
-    # print ('lines', world.getLines())
-    # print ('lines w/o borders', world.getLinesWithoutBorders())
-    # print ('obstacles', world.getObstacles())
-    # drawCross(world.debug, (0, 0))
-    # drawCross(world.debug, (1024, 0))
-    # drawCross(world.debug, (1024, 768))
-    # drawCross(world.debug, (0, 768))
+
     drawCross(world.debug, (620, 690))
     drawCross(world.debug, (628, 698))
     drawCross(world.debug, (635, 705))
     drawCross(world.debug, (608, 684))
     drawCross(world.debug, (980, 484))
-    # drawCross(world.debug, (582, 717))
-    # drawCross(world.debug, (549, 688))
-    # drawCross(world.debug, (554, 546))
-    # drawCross(world.debug, (676, 548))
-    # drawCross(world.debug, (942, 484))
-    # drawCross(world.debug, (811, 396))
-    # drawCross(world.debug, (843, 299))
-    # drawCross(world.debug, (921, 300))
-    # drawCross(world.debug, (457, 422))
-    # drawCross(world.debug, (371, 506))
-    # drawCross(world.debug, (300, 515))
-    # drawCross(world.debug, (300, 400))
-    # drawCross(world.debug, (454, 350))
-    # drawCross(world.debug, (425, 325))
-    # print (cellsize)
-    grid = []
-    # for i in range(maxX):
-    #     grid.append([])
-    #     for j in range(maxY):
-    #         # grid[i].append(True)
-    #         # if insideObstacle((i, j), world.getObstacles()):
-    #         if (isGood((i, j), world, float(cellsize))):
-    #             grid[i].append(True)
-    #         else:
-    #             grid[i].append(False)
 
-    # counter = 0
-    # for i in range(maxX):
-    #     # print (i)
-    #     grid.append([])
-    #     for j in range(maxY):
-    #         # print (i, j, isGood((i, j), world, cellsize))
-    #         # print (grid[counter])
-    #         if (isGood((i, j), world, cellsize)):
-    #             grid[counter].append(True)
-    #         else:
-    #             grid[counter].append(False)
-    #     counter += 1
-    # https://github.gatech.edu/rmendes3/4731_1/blob/master/randomgridnavigator.py
+    grid = numpy.ones((maxX, maxY), dtype=bool)
 
+    int_cellsize = int(cellsize)
     for i in range(maxX):
-        temp = []
         for j in range(maxY):
-            cond = True
-            for obstacle in world.getObstacles():
-                if obstacle.pointInside((i, j)):
-                    cond = False
-            temp.append(cond)
-                # if withinRangeOfPoints((i, j), cellsize, obstacle.getPoints()):
-                    # print ((i, j), 'is within the range of obstacle', obstacle)
-                    # temp.append(False)
-                    # break
-                # else:
-                    # temp.append(True)
-                    # break
-        grid.append(temp)
-    # grid = numpy.array(grid)
-    print (grid[608])
+            if i % int_cellsize == 0 and j % int_cellsize == 0:
+                for obstacle in world.getObstacles():
+                    if obstacle.pointInside((i, j)):
+                    # if withinRangeOfPoints((i, j), cellsize, obstacle.getPoints()):
+                    # if (isGood((i, j), world, cellsize)):
+                        grid[i][j] = False
+    # print (grid[608])
 
     # print (grid[0][0])
     # print ('grid[620][690]', grid[620][690])
